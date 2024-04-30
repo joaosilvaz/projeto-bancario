b385456b55afa9ccb3c0d5d0ad948d2d0bcd6974
João Vitor <joaovitoraz31@gmail.com>
2024-04-30 14:31:46 -0300

    First commit

---
+++ programa.py
@@ -0,0 +1,71 @@
+print("BEM VINDO AO SISTEMA BANCÁRIO JG2 TECH!")
+
+menu = """
+
+[d] Depositar
+[s] Sacar
+[e] Extrato
+[q] Sair
+
+=> """
+
+saldo = 1300
+limite = 500
+extrato = ""
+numero_saques = 0
+LIMITE_SAQUES = 3
+
+while True:
+
+    opcao = input(menu)
+
+    if opcao == "d":
+        valor = float(input("Informe o valor do depósito: "))
+
+        if valor > 0:
+            saldo += valor
+            extrato += f"Depósito: R$ {valor:.2f}\n"
+
+        else:
+            print("Operação falhou! O valor informado é inválido.")
+
+    elif opcao == "s":
+        valor = float(input("Informe o valor do saque: "))
+
+        excedeu_saldo = valor > saldo
+
+        excedeu_limite = valor > limite
+
+        excedeu_saques = numero_saques >= LIMITE_SAQUES
+
+        if excedeu_saldo:
+            print("Operação falhou! Você não tem saldo suficiente.")
+        else:
+            print(f"Operação concluída!")
+
+        if excedeu_limite:
+            print("Operação falhou! O valor do saque excede o limite.")
+
+        if excedeu_saques:
+            print("Operação falhou! Número máximo de saques excedido.")
+
+        if valor > 0:
+            saldo -= valor
+            print(f"Saque: R$ {valor:.2f}")
+            print(f"Saldo atual: {saldo:.2f}")
+            numero_saques += 1
+
+        else:
+            print("Operação falhou! O valor informado é inválido.")
+
+    elif opcao == "e":
+        print("\n================ EXTRATO ================")
+        print("Não foram realizadas movimentações." if not extrato else extrato)
+        print(f"\nSaldo: R$ {saldo:.2f}")
+        print("==========================================")
+
+    elif opcao == "q":
+        break
+
+    else:
+        print("Operação inválida, por favor selecione novamente a operação desejada.")
